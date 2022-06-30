1b46bdb3569c3542000f1e3bd78a6b43e9ed8174
lzpp3 <luizfelippevieira@outlook.com>
2022-06-30 01:55:32 -0300

    test: implements new unit test to health check

--- test/controllers/healthCheckController.spec.ts
+++ test/controllers/healthCheckController.spec.ts
@@ -1,5 +1,13 @@
 import { HealthCheckController } from '../../src/controllers/healthCheckController';
 
+const mockedResponse: any = {
+  json: jest.fn().mockImplementation((result) => {
+    jest.fn().mockReturnValue(result);
+  }),
+  status: jest.fn(() => mockedResponse),
+  setHeader: jest.fn()
+};
+
 describe('HealthCheckController', () => {
   let healthCheckController: HealthCheckController;
 
@@ -9,5 +17,18 @@ describe('HealthCheckController', () => {
 
   it('should be defined', () => {
     expect(healthCheckController).toBeDefined();
+  });
+
+  it.only('should return the correct status code and message', async () => {
+    const mockedResult = {
+      message: 'up running!'
+    };
+
+    const mockedRequest = {};
+
+    await healthCheckController.status(mockedRequest as any, mockedResponse as any);
+
+    expect(mockedResponse.json).toHaveBeenCalledWith(mockedResult);
+    expect(mockedResponse.status).toHaveBeenCalledWith(200);
   });
 });
